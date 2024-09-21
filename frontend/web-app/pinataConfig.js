9cd2a7b7f2c28cbabd2dc36d6f7806b653ea90cb
Aryan <krish7664@gmail.com>
2024-09-22 02:52:41 +0530

    Fix environment vairables

--- frontend/web-app/pinataConfig.js
+++ frontend/web-app/pinataConfig.js
@@ -1,6 +1,6 @@
 import { PinataSDK } from "pinata-web3"
 
 export const pinata = new PinataSDK({
-  pinataJwt: `${import.meta.env.NEXT_PUBLIC_PINATA_JWT}`,
-  pinataGateway: `${import.meta.env.NEXT_PUBLIC_GATEWAY_URL}`
+  pinataJwt: `${process.env.NEXT_PUBLIC_PINATA_JWT}`,
+  pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`
 })
