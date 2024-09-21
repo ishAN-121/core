c3a16860d26a67fee5c0030b777a1086264108d9
demigod <divyanshjain.2206@gmail.com>
2024-09-22 03:54:09 +0800

    change  dircetory structure

--- ignition/modules/ReviewToken.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const ReviewTokenModule = buildModule("ReviewTokenModule", (m) => {
-  const token = m.contract("ReviewToken");
-
-  return { token };
-});
-
-module.exports = ReviewTokenModule;
